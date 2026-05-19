64c6ba2ff38dec75d7f6ef264203058d677555ca
regginssa <randyarabis428@gmail.com>
2026-05-20 07:12:29 +0900

    Add email reset dialog

--- pages/auth/signup/please-verify.tsx
+++ pages/auth/signup/please-verify.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/atoms";
+import { Button, Input } from "@/components/atoms";
 import { OnboardingLayout } from "@/components/layouts/auth/OnboardingLayout";
 import { Icon } from "@iconify/react";
-import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
 
 const PleaseVerify = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <OnboardingLayout>
       <Icon icon="material-symbols-light:mail-shield-outline" width={120} />
@@ -23,9 +33,66 @@ const PleaseVerify = () => {
             classname="font-semibold! rounded-full! text-sm!"
           />
         </div>
-        <Link href="#" className="text-sm underline">
-          Didn't receive email?
-        </Link>
+
+        <Dialog>
+          <DialogTrigger className="text-sm underline cursor-pointer">
+            Didn't receive email?
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-xl">
+                Didn't receive email?
+              </DialogTitle>
+              <DialogDescription className="">
+                Here are some tips to help you find it.
+              </DialogDescription>
+            </DialogHeader>
+
+            <div className="p-4 space-y-6">
+              <ul className="list-decimal text-sm space-y-4">
+                <li>Resend the email</li>
+                <li>
+                  <div>
+                    <p>Search for the email</p>
+                    <p className="text-slate-700 mt-1">
+                      We'll send the email from "WorkLanc", so you can quickly
+                      search for it. If it isn't in your inbox, check your
+                      folders. If a spam filter or email rule moved the email,
+                      it might be in Spam, Junk, Trash, Deleted Items, or
+                      Archive folder.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <p>How do I confirm my email?</p>
+                    <p className="text-slate-700 mt-1">
+                      If you aren't able to click the link, copy the full URL
+                      from email and paste it into a new web browser window.
+                    </p>
+                  </div>
+                </li>
+                <li>Change your email</li>
+              </ul>
+
+              <div className="flex items-center gap-8">
+                <Input
+                  type="email"
+                  placeholder="Email address"
+                  name="email"
+                  value={email}
+                  onChange={(e: any) => setEmail(e.target.value)}
+                />
+              </div>
+              <Button
+                type="outline"
+                size="medium"
+                label="Update and reset"
+                classname="text-sm! rounded-full!"
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </OnboardingLayout>
   );
